ee846191f3ec6551addd2054509122b71092a06e
Micah Young <micah@young.io>
2014-10-26 22:29:39 +0000

    Clean up invalid file

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -7,4 +7,4 @@ cd $JSONDIR
 curl --retry 30 http://appservices.citibikenyc.com/data2/stations.php?updateOnly=true > $FILE
 git add -A .
 git commit -m`date +%s`
-test -n "$1" && git push
+test -n "$1" && git push && git gc --auto
